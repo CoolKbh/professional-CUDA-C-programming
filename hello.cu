8f202956230680257ca8099befda69982967d46b
CoolKbh <hm13718556238@126.com>
2022-11-09 11:31:06 +0800

    Hello GPU!

--- hello.cu
+++ hello.cu
@@ -1,4 +1,15 @@
 #include<stdio.h>
+
+__global__ void helloFromGPU(void){
+    printf("Hello world from GPU!");
+}
+
 int main(void){
-    printf("Hello world from CPU\n");
+    //hello from CPU
+    printf("Hello world from CPU!\n");
+
+    //hello from GPU
+    helloFromGPU<<<1, 10>>>();
+    cudaDeviceReset();
+    return 0;
 }
