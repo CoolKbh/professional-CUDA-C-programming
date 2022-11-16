b1f1261ed8a0afb6dcc3fa73d768aba7755a030d
kongbohua_docker <hm13718556238@126.com>
2022-11-16 09:10:00 +0000

    sumOnGPU

--- sumArraysOnHost.c
+++ sumArraysOnHost.c
@@ -21,10 +21,19 @@ void initialData(float *ip, int size){
 int main() {
     int nElem = 1024;
     size_t nBytes = nElem * sizeof(float);
+    
     float *h_A, *h_B, *h_C;
     h_A = (float*)malloc(nBytes);
     h_B = (float*)malloc(nBytes);
     h_C = (float*)malloc(nBytes);
+
+    float *d_A, *d_B, *d_C;
+    cudaMalloc((float**)&d_A, nBytes);
+    cudaMalloc((float**)&d_B, nBytes);
+    cudaMalloc((float**)&d_C, nBytes);
+
+    cudaMemcpy(d_A, h_A, nBytes, cudaMemcpyHostToDevice);
+    cudaMemcoy(d_B, h_B, nBytes, cudaMemcpyHostToDevice):
 
     initialData(h_A, nElem);
     initialData(h_B, nElem);
